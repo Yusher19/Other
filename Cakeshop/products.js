5ea79aa5212341f9f45cb13789ce0e47d70405d0
Yusher19 <62275674+Yusher19@users.noreply.github.com>
2023-03-27 19:24:36 +0800

    Add files via upload

---
+++ Cakeshop/products.js
@@ -0,0 +1,86 @@
+const products = [
+    {
+        title: "Chocolate Cake",
+        description:"Chocolate Cake with Chocolate Syrup with Sprinkles",
+        image: 'images/cake1.jpg',
+        price: "250 pesos",
+    },
+    {
+        description:"Strawberry Cupcake with Vanilla Syrup",
+        image: 'images/cake2.jpg',
+        price: 300,
+    },
+    {
+        description:"Ice Cream Cake with Chocolate Syrup",
+        image: 'images/cake3.jpg',
+        price: 350,
+    },
+    {
+        description:"Fruit Cake",
+        image: 'images/cake4.jpg',
+        price: 250,
+        
+    },
+    {
+        description:"Blueberry Cupcakes",
+        image: 'images/cake5.jpg',
+        price: 250, 
+    },
+    {
+        description:"Brown Cake",
+        image: 'images/cake6.jpg',
+        price: 300,
+    },
+    {
+        description:"Chocolate Donuts",
+        image: 'images/cake7.jpg',
+        price: 300,
+    },
+    {
+        description:"Vanilla Cake",
+        image: 'images/cake8.jpg',
+        price: 300,
+    },
+    {
+        description:"Ice Cream Cake with Chocolate Syrup",
+        image: 'images/cake9.jpg',
+        price: 300,
+    },
+    {
+        description:"Cupcake with Marshmallow",
+        image: 'images/cake10.jpg',
+        price: 300,
+    },
+    {
+        description:"Strawberry Cupcakes",
+        image: 'images/cake11.jpg',
+        price: 300,
+    },
+    {
+        description:"Cake with Strawberry Toppings",
+        image: 'images/cake12.jpg',
+        price: 300,
+    },
+    {
+        description:"Cheese Cake",
+        image: 'images/cake13.jpg',
+        price: 300,
+    },
+    {
+        description:"Strawberry Cake",
+        image: 'images/cake14.jpg',
+        price: 300,
+    },
+    {
+        description:"Carrot and Wallnut Cake",
+        image: 'images/cake15.jpg',
+        price: 300,
+    },
+    {
+        description:"Lemon Yogurt Cake",
+        image: 'images/cake16.jpg',
+        price: 300,
+    },
+    
+
+];
