3eb20ce6ca0ca25bf80f5dbd619c3c4652edb613
Yusher19 <62275674+Yusher19@users.noreply.github.com>
2023-03-29 15:48:09 +0800

    Add files via upload

--- load.js
+++ load.js
@@ -20,18 +20,17 @@ const jsonToHTML =
   (json) => `
   <div itemscope itemtype="https://schema.org/Product" class="card">
     <img itemprop="image" src=${json.image} alt="Avatar" style="width:100%"/>
+ 
     <div class="container">
       <h4 itemprop="description"><b>${json.description}</b></h4>
-      <p>
+ 
       <span itemprop="offers" itemscope itemtype="https://schema.org/Offer">
         <span itemprop="price">${json.price} Pesos</span>
-        <form id="textBox">
-        <label for="textproduct"></label>
-        <input id="text" name="filter" type="search...">
-        <button type="submit">Customize</button>
-      </form>
       </span>
-      <p>
+      
+        <input name="filter" type="text" placeholder="Type customization here">
+        <button id="btn2" type="submit">Customize</button>
+ 
     </div>
   </div>
   `
