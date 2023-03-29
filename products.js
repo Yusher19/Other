bc6a0563a98e628896ad10eaa51fbba15cc7ab39
Yusher19 <62275674+Yusher19@users.noreply.github.com>
2023-03-30 04:46:19 +0800

    Add files via upload

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products = [
         title: "Chocolate Cake",
         description:"Chocolate Cake with Chocolate Syrup with Sprinkles",
         image: 'images/cake1.jpg',
-        price: 250,
+        price: 450,
     },
     {
         description:"Strawberry Cupcake with Vanilla Syrup",
@@ -13,12 +13,12 @@ const products = [
     {
         description:"Ice Cream Cake with Chocolate Syrup",
         image: 'images/cake3.jpg',
-        price: 350,
+        price: 450,
     },
     {
         description:"Fruit Cake",
         image: 'images/cake4.jpg',
-        price: 250,
+        price: 400,
         
     },
     {
@@ -34,17 +34,17 @@ const products = [
     {
         description:"Chocolate Donuts",
         image: 'images/cake7.jpg',
-        price: 300,
+        price: 250,
     },
     {
         description:"Vanilla Cake",
         image: 'images/cake8.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Ice Cream Cake with Chocolate Syrup",
         image: 'images/cake9.jpg',
-        price: 300,
+        price: 500,
     },
     {
         description:"Cupcake with Marshmallow",
@@ -59,97 +59,97 @@ const products = [
     {
         description:"Cake with Strawberry Toppings",
         image: 'images/cake12.jpg',
-        price: 300,
+        price: 450,
     },
     {
         description:"Cheese Cake",
         image: 'images/cake13.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Strawberry Cake",
         image: 'images/cake14.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Carrot and Wallnut Cake",
         image: 'images/cake15.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Lemon Yogurt Cake",
         image: 'images/cake16.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Chocolate Mud Cupcake",
         image: 'images/cake17.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Flourless Orange Cake",
         image: 'images/cake18.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Vanilla Cupcakes",
         image: 'images/cake19.jpg',
-        price: 300,
+        price: 350,
     },
     {
         description:"Sponge Cake",
         image: 'images/cake20.jpg',
-        price: 300,
+        price: 350,
     },
     {
         description:"Chocolate Birthday Cake",
         image: 'images/cake21.jpg',
-        price: 300,
+        price: 450,
     },
     {
         description:"Classic No Bake Vanilla",
         image: 'images/cake22.jpg',
-        price: 300,
+        price: 500,
     },
     {
         description:"Gluten-Free Persian Cake",
         image: 'images/cake23.jpg',
-        price: 300,
+        price: 500,
     },
     {
         description:"Red Velvet Cupcakes",
         image: 'images/cake24.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Baked Lemon Cheese Cake",
         image: 'images/cake25.jpg',
-        price: 300,
+        price: 500,
     },
     {
         description:"Humming Bird Cake",
         image: 'images/cake26.jpg',
-        price: 300,
+        price: 400,
     },
     {
         description:"Marble Cake",
         image: 'images/cake27.jpg',
-        price: 300,
+        price: 500,
     },
     {
         description:"Flourless Chocolate Cake",
         image: 'images/cake28.jpg',
-        price: 300,
+        price: 500,
     },
     {
         description:"Black Forest Cake",
         image: 'images/cake29.jpg',
-        price: 300,
+        price: 600,
     },
     {
         description:"Layered Rainbow Cake",
         image: 'images/cake30.jpg',
-        price: 300,
+        price: 600,
     },
 
 ];
