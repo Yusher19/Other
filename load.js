89c4a707123948345610648e7c80f49a8dade23a
Yusher19 <62275674+Yusher19@users.noreply.github.com>
2023-03-30 01:12:37 +0800

    Add files via upload

--- load.js
+++ load.js
@@ -1,9 +1,11 @@
 const filterProducts =
         (products, searchTerm) => {
-                const regex = new RegExp(searchTerm,"i")
-                const displayedProducts = products.filter( x => x.description.replace(" ","").match(regex))
-                document.querySelector("#products").innerHTML = displayedProducts.map(jsonToHTML).join('')
-                return displayedProducts
+          console.log("filterProducts called with searchTerm: " +  searchTerm + " and array length: " + products.length)
+          console.log(products)
+          const fuse = new Fuse(products, { keys: Object.keys(products[0]), threshold: 0.1})
+          const displayedProducts = fuse.search(searchTerm)
+          console.log("filtered to this number of items: " + displayedProducts.length)
+          document.querySelector("#products").innerHTML = displayedProducts.map(x=>x.item).map(jsonToHTML).join('')
   }
  
 const displayFilteredProducts = 
@@ -16,6 +18,9 @@ const displayFilteredProducts =
 const filterForm = document.querySelector("#filterForm")
 filterForm.addEventListener( "submit", displayFilteredProducts )
  
+const generateJsonDigest =
+  (json) => nacl.util.encodeBase64(nacl.hash(nacl.util.decodeUTF8(canonicalize(json))))
+ 
 const jsonToHTML =
   (json) => `
   <div itemscope itemtype="https://schema.org/Product" class="card">
@@ -25,14 +30,11 @@ const jsonToHTML =
       <h4 itemprop="description"><b>${json.description}</b></h4>
  
       <span itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-        <span itemprop="price">${json.price} Pesos</span>
+        <span itemprop="price" content="${json.price}">${json.price} Pesos</span>
       </span>
       
         <input name="filter" type="text" placeholder="Type customization here">
-        <button id="btn2" type="submit">Customize</button>
-        <div>
-        <button id="btn3" type="submit">Add to Cart</button>
-        </div>
+        <button id="button-${generateJsonDigest(json)}" type="submit">Customize</button>
  
     </div>
   </div>
@@ -40,3 +42,36 @@ const jsonToHTML =
  
 window.onload =
   () => document.querySelector("#products").innerHTML = products.map(jsonToHTML).join('')
+
+  const fetchImage =
+  async (prompt) => {
+    var response = await fetch("./api/image/generate?prompt=" + prompt)
+    if ( response.ok ) {
+      console.log("successly generated image")
+      const url = (await response.json())[0]
+      console.log(url)
+      return url
+    } else {
+      return "failed"
+    }
+  }
+ 
+const generateCustomImage = 
+  async (event) => {
+    if(event.target.nodeName === "BUTTON") {
+      const productElement = event.target.closest("[itemtype='https://schema.org/Product']")
+      const originalDescription = productElement.querySelector("[itemprop='description']").textContent
+      const prompt = (originalDescription + ", " + productElement.querySelector("input").value).replace(/, ,/,",")
+      const price = productElement.querySelector("[itemprop='price']").getAttribute("content")
+      const url = await fetchImage(prompt)
+      products.push(
+        { description: prompt
+        , price: price
+        , image: url
+        })
+      filterProducts(products, originalDescription)
+    }
+  }
+ 
+const productSection = document.querySelector("#products")
+productSection.addEventListener("click",generateCustomImage)
